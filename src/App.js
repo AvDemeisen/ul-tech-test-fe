67f8dcbb62247c00090f3bf211210465052d8c32
ddevine <ddevine@MacBook-Pro.local>
2020-07-18 09:43:06 +0100

    ist products

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState, useEffect } from "react";
+import './App.scss';
+
+const App = () => {
+
+	const [products, setProducts] = useState();
+
+	useEffect(() => {
+
+		const fetchProducts = () => {
+			fetch(`https://sleepy-depths-24610.herokuapp.com/products`)
+				.then(response => response.json())
+				.then(jsonData => setProducts(jsonData))
+				.catch(err => console.log(err))
+		}
+
+		fetchProducts()
+	}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='section__body'>
+				{products ? <div className="section__inner">
+				<div className="products-provider"><h2>ProductList</h2></div>
+					{products.map(article => <div key={article.id}>{article.name}</div>)}
+				</div> : null}
+			</div>
     </div>
   );
 }
